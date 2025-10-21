2c159078b080796d4c5a2fe7b2f120cad2f4c818
Liu2dog <g3148198932@163.com>
2025-10-21 21:05:46 +0800

    Add files via upload

---
+++ 答案1.py
@@ -0,0 +1,58 @@
+# 1.C
+# 2.B
+# 3.C
+# 4.C
+# 5.C
+# 6.B
+# 7.A
+# 8.C
+
+
+# #1.
+# chinese = float(input('你的语文成绩是：'))
+# math = float(input('你的数学成绩是：'))
+# english = float(input('你的英语成绩是：'))
+# sum = chinese+math+english
+# average = sum/3
+# print('你的总成绩是：%.1f'%sum)
+# print('你的平均分是：%.1f'%average)
+#
+# #2.
+# #1)
+# name = '小明'
+# primary = 175
+# discount = 0.75
+# present = '?'
+# #2)
+# present = float(primary*discount)
+# print('打折后毛衣的价格为：%.1f'%present)
+
+# #3
+# a = 1 == 2
+# b = 8 / 3 == 2
+# c = a or b
+# d = not c
+# e = 2*2**3 == 64
+# f = d and e
+# print('第一题结果为：',f)
+# g = 1+2==2
+# h = 3*3
+# i = g or h
+# j = i and True
+# print('第二题的结果是：',j)
+
+
+# #4
+# num1 = int(input('第一个数字是：'))
+# num2 = int(input('第二个数字是：'))
+# op=input('运算符号是：')
+# sum=num1+num2
+# print(f'计算结果是；{sum}')
+
+
+#5
+# 1）  浮点型
+# 2）  字符型
+# 3）  17.5
+# 4）  报错
+# 5）  4.0a
